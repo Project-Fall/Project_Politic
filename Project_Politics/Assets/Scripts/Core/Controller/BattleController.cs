a87c95ef60d207b2475f1f6347d371ce191ffbad
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-26 16:24:56 +0900

    bug : SO 설정 추가

--- Project_Politics/Assets/Scripts/Core/Controller/BattleController.cs
+++ Project_Politics/Assets/Scripts/Core/Controller/BattleController.cs
@@ -2,11 +2,13 @@
 
 public class BattleController : IController
 {
-    public Character[] Candidates { get;}
+    public Character[] Candidates { get; }
 
     public BattleController()
     {
         Candidates = new Character[2];
+        Candidates[0] = Managers.Resource.Load<Character>("ScriptObjects/Character/NPC1");
+        Candidates[1] = Managers.Resource.Load<Character>("ScriptObjects/Character/Player");
     }
 
     public Character GetResultCandidate()
