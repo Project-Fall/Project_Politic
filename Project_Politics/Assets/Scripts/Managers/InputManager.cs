33b21e581907e7b6795672a33789ea8338f9dc34
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-12-02 02:22:11 +0900

    feature : Input Manager 추가
    더블클릭 버그 해결

---
+++ Project_Politics/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    public bool Click { get { return OneClick(); } }
+
+    private long _lastClickTime = 0;
+    public bool OneClick()
+    {
+        if (Input.GetMouseButtonDown(0) && DateTime.Now.Ticks - _lastClickTime > 2000000)
+        {
+            _lastClickTime = DateTime.Now.Ticks;
+            return true;
+        }
+
+        return false;
+    }
+}
