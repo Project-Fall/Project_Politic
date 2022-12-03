148f0be0dcf6418dded20f48feb8a75e82c5b39a
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2022-12-03 18:04:54 +0900

    커스터마이징 시스템 스크립트 추가
    커스터마이징 시스템 스크립트 추가

--- Project_Politics/Assets/Scripts/MotionTest/PlayerMotionManager.cs
+++ Project_Politics/Assets/Scripts/MotionTest/PlayerMotionManager.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class PlayerMotionManager : MonoBehaviour
 {
+    public static PlayerMotionManager Instance // Sington 나중에 수정해도 ㄱㅊ
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                instance = FindObjectOfType<PlayerMotionManager>();
+                if(Instance == null)
+                {
+                    var instanceContainer = new GameObject("PlayerMotionManager");
+                    instance = instanceContainer.AddComponent<PlayerMotionManager>();
+                }
+            }
+            return Instance;
+        }
+    }
+
+    private static PlayerMotionManager instance;
+
+    public PlayerSkin[] PlayerSkins;
+
+    public void SkinSetTrigger(string AniName)
+    {
+        foreach(PlayerSkin skin in PlayerSkins)
+        {
+            skin.anim.SetTrigger(AniName);
+        }
+    }
+
+
 
 }
