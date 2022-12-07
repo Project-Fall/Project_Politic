0a4d40c4f816bbd287125ae2d2fb359b914a3817
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-12-07 20:41:48 +0900

    feature : 가구 이미지 전부 가져오기

--- Project_Politics/Assets/Scripts/UI/Scene/UI_Deploy.cs
+++ Project_Politics/Assets/Scripts/UI/Scene/UI_Deploy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +8,8 @@ public class UI_Deploy : UI_Scene
 {
     public class Item
     {
-        string name;
-        Image image;
+        public string name;
+        public Sprite image;
     }
 
     enum Objects
@@ -16,18 +17,31 @@ public class UI_Deploy : UI_Scene
         Items
     }
 
-    public List<Sprite> items = new List<Sprite>();
+    public List<Item> items = new List<Item>();
 
     private void Start()
     {
         Bind<GameObject>(typeof(Objects));
-        items.Add(Managers.Resource.Load<Sprite>("Image/Sofa"));
-        items.Add(Managers.Resource.Load<Sprite>("Image/Chair"));
+        Init();
+    }
 
-        foreach (Sprite item in items)
+    private void Init()
+    {
+        // 나중에 데이터에서 관리
+        string pattern = "*.png";
+        string[] names = Directory.GetFiles("Assets/Resources/Art/Sprites/Office_Object/Furniture", pattern);
+        foreach (string n in names)
         {
+            string name = Path.GetFileNameWithoutExtension(n);
+            Item item = new Item();
+            item.image = Managers.Resource.Load<Sprite>($"Art/Sprites/Office_Object/Furniture/{name}");
+            if (item.image == null)
+                continue;
+
+            item.name = name;
+            items.Add(item);
             GameObject go = Managers.Resource.Instantiate("UI/Item", GetObject(0).transform);
-            go.GetComponent<Image>().sprite = item;
+            go.GetComponent<Image>().sprite = item.image;
         }
     }
 }
