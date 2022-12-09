6d535b166a8788a56b43b87f9a427d37b5f9b1c2
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-12-09 17:24:08 +0900

    feature : 옵션(세팅)창 추가

--- Project_Politics/Assets/Scripts/UI/Popup/UI_Option.cs
+++ Project_Politics/Assets/Scripts/UI/Popup/UI_Option.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UI_Option : UI_Popup
 {
-    // Start is called before the first frame update
+    enum Objects
+    {
+        ClickBlocker,
+        CloseButton,
+    }
+    
     void Start()
     {
-        
+        Bind<GameObject>(typeof(Objects));
+
+        // X 버튼을 누르거나, 창 바깥을 누를 때 꺼짐
+        BindEvent(GetObject((int)Objects.ClickBlocker), (PointerEventData) => Close());
+        BindEvent(GetObject((int)Objects.CloseButton), (PointerEventData) => Close());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Close()
     {
-        
+        Managers.UI.ClosePopup(this);
     }
 }
