16d71c374e28e817074e5c5d9446652e84247114
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-12-10 02:24:43 +0900

    fix : 선거 상대가 틀린 문제 수정

--- Project_Politics/Assets/Scripts/Core/Controller/BattleController.cs
+++ Project_Politics/Assets/Scripts/Core/Controller/BattleController.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 public class BattleController : IController
 {
-
     public Character[] Candidates { get; }
 
     public BattleController()
     {
         Candidates = new Character[2];
-        Candidates[0] = Managers.Resource.Load<Character>($"ScriptObjects/Character/NPC/NPC{Random.Range(0, 3)+1}");
-        Candidates[1] = Managers.Resource.Load<Character>("ScriptObjects/Character/Player");
+        Managers.Data.Init();
+        Candidates[0] = Managers.Data.NPCs[Random.Range(0, Managers.Data.NPCs.Count)];
+        Candidates[1] = Managers.Data.Player;
     }
 
-    public Character GetResultCandidate()
+    public Character GetWinner()
     {
         if (Candidates[0].Awareness > Candidates[1].Awareness)
         {
             return Candidates[0];
         }
         return Candidates[1];
-    }
-
-    public Character GetCandidate(int idx)
-    {
-        return Candidates[idx];
     }
 }
