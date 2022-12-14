355eb270fd6b470986fd5cff05912e247d3744b8
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-12-15 01:21:04 +0900

    feature : Button SE 추가

---
+++ Project_Politics/Assets/Scripts/UI/Sub/UI_Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Button : UI_Base
+{
+    [SerializeField] AudioClip ClickSE;
+    [SerializeField] AudioClip HoverSE;
+    [SerializeField] bool UseSE = true;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (UseSE)
+        {
+            if (ClickSE == null)
+                BindEvent(gameObject, (PointerEventData) => { Managers.Sound.Play("ButtonClick"); });
+            else
+                BindEvent(gameObject, (PointerEventData) => { Managers.Sound.Play(ClickSE); });
+
+            if (HoverSE == null)
+                BindEvent(gameObject, (PointerEventData) => { Managers.Sound.Play("ButtonHover"); }, Define.UIEvent.Hover);
+            else
+                BindEvent(gameObject, (PointerEventData) => { Managers.Sound.Play(HoverSE); }, Define.UIEvent.Hover);
+        }
+    }
+}
